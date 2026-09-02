e436bd3080da7dbe7034c2a297a069de2c640211
Nsoro-Allan <nsoroallan@gmail.com>
2026-09-03 00:06:17 +0200

    Chore: Android build init.

---
+++ src-tauri/gen/android/app/src/main/java/com/nsoroallan/localvox/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nsoroallan.localvox
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
